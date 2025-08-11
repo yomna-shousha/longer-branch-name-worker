f771e9a39d430f20e167b136ee5c3889d31c3336
yomna <yomna@cloudflare.com>
2025-08-11 11:14:19 -0400

    Test 63-character total length

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 
 export default {
   async fetch(request, env, ctx) {
-    return new Response("Hello from bugfix branch - memory leak fix!", {
+    return new Response("Hello from 63-char total!", {
       headers: { "Content-Type": "text/plain" }
     });
   },
