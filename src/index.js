5351f5a58320a814f96eb7f5999183038badb343
yomna <yomna@cloudflare.com>
2025-08-11 11:25:24 -0400

    Test 70+ character total length

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 
 export default {
   async fetch(request, env, ctx) {
-    return new Response("Hello from 63-char total!", {
+    return new Response("Hello from 70+ char total!", {
       headers: { "Content-Type": "text/plain" }
     });
   },
