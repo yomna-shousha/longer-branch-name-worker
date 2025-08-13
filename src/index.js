e0ba4ef138cf6c1f1aea9c7b084156c91f5d4987
yomna <yomna@cloudflare.com>
2025-08-13 09:56:19 -0400

    Test 65 chars total with updated Wrangler

--- src/index.js
+++ src/index.js
@@ -1,7 +1,1 @@
-export default {
-  async fetch(request, env, ctx) {
-    return new Response("65 chars total - should use hash mode", {
-      headers: { "Content-Type": "text/plain" }
-    });
-  },
-};
+export default { async fetch(request, env, ctx) { return new Response("65 chars total - should use hash new lol", { headers: { "Content-Type": "text/plain" } }); }, };
